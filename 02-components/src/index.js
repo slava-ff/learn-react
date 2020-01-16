7db9a80df1bedd8325a1b4167abe727a25f08862
s.famin <s.famin@godeltech.com>
2020-01-16 19:14:39 +0300

    finished 3rd section

--- 02-components/src/index.js
+++ 02-components/src/index.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
-        <div className="ui container comment">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure?
                 </div>
-            </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author="Sam"
+                    timeAgo ="Today at 4:45PM"
+                    comment={faker.random.words()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author="Alex"
+                    timeAgo ="Today at 2:00AM"
+                    comment={faker.random.words()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author="Jane"
+                    timeAgo ="Yesterday at 11:00AM"
+                    comment={faker.random.words()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
