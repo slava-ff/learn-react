8b1176ca1a7546f0c0dfa9e6145811cc9ca7021c
s.famin <s.famin@godeltech.com>
2020-01-29 19:33:49 +0300

    finished 6 chapter

--- 03-seasons/src/index.js
+++ 03-seasons/src/index.js
@@ -13,11 +13,7 @@ class App extends React.Component {
         );
     }
 
-    componentDidUpdate() {
-        console.log("DID UPDATE");
-    }
-
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -28,6 +24,12 @@ class App extends React.Component {
         
         return <Spinner message="Please accept location request" />;
     }
+
+    render() {
+        <div className="border red">
+            {this.renderContent()}
+        </div>
+    }  
 }
 
 ReactDOM.render(
