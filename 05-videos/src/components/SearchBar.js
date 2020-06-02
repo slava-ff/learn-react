8fcc3618b145c75ea6bf692310fb3beabe666a07
s.famin <s.famin@godeltech.com>
2020-06-03 02:21:50 +0300

    122

--- 05-videos/src/components/SearchBar.js
+++ 05-videos/src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
     onFormSubmit = event => {
         event.preventDefault();
 
-        // make call from a parent component
+        this.props.onFormSubmit(this.state.term);
     }
 
     render() {
